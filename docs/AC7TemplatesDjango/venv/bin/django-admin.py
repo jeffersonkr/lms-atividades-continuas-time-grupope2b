8c64f31d022434639bdd944c55b77b74cb736790
Yuri Dirickson <ydirickson@gmail.com>
2018-04-21 13:43:18 -0300

    Correção AC7

---
+++ docs/AC7TemplatesDjango/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ydirickson/Desenvolvimento/git/FIT/TecWeb/2018-1/GrupoPE2B/docs/AC7TemplatesDjango/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
