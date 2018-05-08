58a3da333a192c21814d5feba77d5013c4cd8c84
jeffersonkr <jeffersonkr@hotmail.com>
2018-05-08 11:33:20 -0300

    Adicionado htmls graduacao

--- docs/core/urls.py
+++ docs/core/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from core.views import home, formulario
+from core.views import *
 
 
 
 urlpatterns = [
     path('', home),
-    path('ListaAprovacao/',formulario,),
+    path('ListaAprovacao/', formulario),
+    path('graduacao/', graduacao),
+    path('ads/', ads),
+    path('bd/', bd),
+    path('gti/', gti),
+    path('jogosdigitais/', jogosdigitais),
+    path('administracao/', adm),
 ]
--- docs/core/views.py
+++ docs/core/views.py
@@ -6,4 +6,20 @@ def home(request):
 def formulario(request):
     return render(request, 'formulario.html')
 
+def graduacao(request):
+    return render(request, 'graduacao.html')
 
+def adm(request):
+    return render(request, 'adm.html')
+
+def bd(request):
+    return render(request, 'bd.html')
+
+def jogosdigitais(request):
+    return render(request, 'jogosdigitais.html')
+
+def gti(request):
+    return render(request, 'gti.html')
+
+def ads(request):
+    return render(request, 'ads.html')
