47fe4f30f820dbeaa1e088ce7298d44d78e16210
jeffersonkr <jeffersonkr@hotmail.com>
2018-05-15 12:56:55 -0300

    alterado gerador de RA

--- docs/AC7TemplatesDjango/settings.py
+++ docs/AC7TemplatesDjango/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/docs/core/static/'
 
-#try:
-#    from .local_settings import *
-#except Error:
-#    pass
+try:
+    from .local_settings import *
+except Error:
+    pass
